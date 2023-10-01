e3fdfdb38fd163a71d9619118f716873873d7cf1
Daniel Kamil Kozar <dkk089@gmail.com>
2023-10-02 01:23:08 +0200

    huh

--- .github/workflow_build_windows.bat
+++ .github/workflow_build_windows.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-cl /std:c++20 /O2 /W4 /MT /arch:IA32 /utf-8 /permissive- cachex.cpp
+cl /std:c++20 /O2 /W4 /MT /arch:IA32 /utf-8 /permissive- /EHsc cachex.cpp
