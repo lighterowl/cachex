b2f292911e9558591d6a56174793b265f32bbc33
Daniel Kamil Kozar <dkk089@gmail.com>
2023-11-23 21:50:20 +0100

    save the artifact from Windows action

--- .github/workflow_build_windows.bat
+++ .github/workflow_build_windows.bat
@@ -1,2 +1,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 cl /std:c++20 /O2 /W4 /MT /arch:IA32 /utf-8 /permissive- /EHsc cachex.cpp
+7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -sse cachex.7z cachex.exe
+certutil -hashfile cachex.7z SHA256
